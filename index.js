ccc5e90bc374ed47b9ac84c392e601bb69b4e62b
Googler <knowbeeinc@gmail.com>
2020-03-03 21:42:36 +0200

    proxy can be removed

--- index.js
+++ index.js
@@ -3,56 +3,59 @@ const { exec } = require("child_process");
 const clear = require("clear");
 const chalk = require("chalk");
 const figlet = require("figlet");
-const cmd = require("node-cmd");
 const ora = require("ora");
 const spinner = ora();
 clear();
 console.log(
-  chalk.green(figlet.textSync("proxier", { horizontalLayout: "full" }))
+  chalk.magenta(figlet.textSync("proxier", { horizontalLayout: "full" }))
 );
 
 re = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?:[0-9]+$/;
-const lastpattern = process.argv.slice(-1)[0];
-
-if (lastpattern === "remove" || lastpattern === "--remove") {
-  if (name === "npm" || name === "--npm") {
-    cmd.run("npm config delete https-proxy");
-  }
-  if (name === "git" || name === "--git") {
-    cmd.run("git config --global --unset https.proxy");
-  }
-  if (name === "yarn" || name === "--yarn") {
-    cmd.run("yarn config delete https-proxy");
-  }
-  // spinner.succeed("proxy settings removed");
-  process.exit(1);
-}
-
-if (!re.test(lastpattern)) {
-  spinner.fail(`proxy format is not valid`);
-  process.exit(1);
-}
-
-// // console.log(process.argv.slice(2, -1));
-
-process.argv.slice(2, -1).forEach(name => {
-  if (name === "npm" || name === "--npm") {
-    proxier(`npm config set https-proxy ${lastpattern}`);
-    console.log("npm proxy set");
-  }
-  if (name === "git" || name === "--git") {
-    proxier(`git config --global http.proxy ${lastpattern}`);
-    console.log("git proxy set");
-  }
-  if (name === "yarn" || name === "--yarn") {
-    proxier(`yarn config set https-proxy ${lastpattern}`);
-    console.log("yarn proxy set");
-  }
-});
-
-function proxier(setting) {
+const proxy = process.argv.slice(-1)[0];
+const pattern = process.argv[2];
+if (pattern === "remove" || pattern === "--remove") {
+  process.argv.slice(3).forEach(name => {
+    if (name === "npm" || name === "--npm") {
+      exec("npm config delete https-proxy");
+      exec("npm config delete proxy");
+    }
+    if (name === "yarn" || name === "--yarn") {
+      exec("yarn config delete proxy");
+    }
+    if (name === "git" || name === "--git") {
+      exec("git config --unset-all http.proxy");
+    }
+  });
+  console.log();
+  spinner.succeed("proxy removed");
+} else if (pattern === "set" || (pattern === "--set" && re.test(proxy))) {
   try {
-    cmd.run(setting);
+    process.argv.slice(2, -1).forEach(name => {
+      if (name === "npm" || name === "--npm") {
+        proxier(`npm config set https-proxy ${proxy}`);
+      }
+      if (name === "git" || name === "--git") {
+        proxier(`git config --add http.proxy ${proxy}`);
+      }
+      if (name === "yarn" || name === "--yarn") {
+        proxier(`yarn config set https-proxy ${proxy}`);
+      }
+    });
+    console.log();
+    spinner.succeed("proxy added");
+  } catch (error) {
+    console.log(error);
+  }
+} else if (!re.test(proxy)) {
+  spinner.fail(`proxy format supported is: http|https://host:port`);
+  process.exit(1);
+} else {
+  spinner.fail(`run: proxier --help`);
+}
+
+function proxier(config) {
+  try {
+    exec(config);
   } catch (error) {
     console.log(error);
   }
